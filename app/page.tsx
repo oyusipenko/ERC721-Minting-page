13f459fcfabf1266e997894d0693a7ef6516de68
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2025-01-14 20:17:36 +0100

    feat(ui): enhance ERC721 minting page with layout and MintForm component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
 "use client";
 
+import { MintForm } from "@/src/components";
+
 export default function Home() {
-  return <div>Content</div>;
+  return (
+    <div className="flex flex-col items-center p-10 ">
+      <div className="flex flex-col items-center justify-center border-2 rounded-lg p-20">
+        <h1 className="prose-2xl prose-slate">Mint New NFT</h1>
+        <h5 className="prose-lg prose-slate">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sem
+          tortor quis amet scelerisque vivamus egestas.{" "}
+        </h5>
+      </div>
+      <MintForm />
+    </div>
+  );
 }
