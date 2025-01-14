3cd38660ebef91ac5a1cded4ccf111dd234bc319
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2025-01-14 20:53:28 +0100

    feat(ui): update Sidebar component to include Profile component and adjust styling

--- src/components/templates/SideBar.tsx
+++ src/components/templates/SideBar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useSidebar } from "@/src/context/SidebarContext";
+import { useSidebar } from "@/src/context/";
+import { Profile } from "@/src/components/";
 
 interface SidebarProps {
   isOpen: boolean;
@@ -12,7 +13,7 @@ export default function Sidebar({ isOpen }: SidebarProps) {
   return (
     <div
       className={`
-        fixed top-0 right-0 w-128 h-full bg-gray-800 text-white 
+        fixed top-0 right-0 w-96 h-full bg-gray-800 text-white 
         transform transition-transform duration-300
         ${isOpen ? "translate-x-0" : "translate-x-full"}
       `}
@@ -27,8 +28,7 @@ export default function Sidebar({ isOpen }: SidebarProps) {
           </button>
         </div>
         <div className="p-4">
-          <h2 className="text-xl font-bold">Sidebar</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <Profile />
         </div>
       </div>
     </div>
